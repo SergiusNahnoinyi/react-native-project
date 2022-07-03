029a778eb289e784b227de1b1116ba56cbeeaac9
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-03 11:58:13 +0300

    Addes initial form markup

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function RegistrationScreen() {
   return (
@@ -7,7 +14,22 @@ export default function RegistrationScreen() {
       <ImageBackground
         source={require("../assets/images/backgroung.jpg")}
         style={styles.image}
-      ></ImageBackground>
+      >
+        <View style={styles.form}>
+          <Text style={styles.title}>Registration</Text>
+          <TextInput style={styles.input} placeholder="Login" />
+          <TextInput style={styles.input} placeholder="Email" />
+          <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="Password"
+          />
+          <Button title={"Sign up"} style={styles.button} />
+          <Text style={styles.text}>
+            Do you already have an account? Sign in
+          </Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -19,5 +41,11 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: "cover",
+    justifyContent: "flex-end",
   },
+  form: {},
+  title: {},
+  input: {},
+  button: {},
+  text: {},
 });
