c795f9e503be039674f3ba1dafa14af8e4d97b95
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-03 14:01:30 +0300

    Implements styles and changes button component to the touchable opacity component
    Standart button component in React Native can not be styled as it is required

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -5,7 +5,7 @@ import {
   ImageBackground,
   Text,
   TextInput,
-  Button,
+  TouchableOpacity,
 } from "react-native";
 
 export default function RegistrationScreen() {
@@ -17,14 +17,16 @@ export default function RegistrationScreen() {
       >
         <View style={styles.form}>
           <Text style={styles.title}>Registration</Text>
-          <TextInput style={styles.input} placeholder="Login" />
+          <TextInput style={styles.input} placeholder="Name" />
           <TextInput style={styles.input} placeholder="Email" />
           <TextInput
             style={styles.input}
             secureTextEntry={true}
             placeholder="Password"
           />
-          <Button title={"Sign up"} style={styles.button} />
+          <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+            <Text style={styles.buttonText}>Sign up</Text>
+          </TouchableOpacity>
           <Text style={styles.text}>
             Do you already have an account? Sign in
           </Text>
@@ -40,12 +42,49 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    resizeMode: "cover",
     justifyContent: "flex-end",
+    resizeMode: "cover",
   },
-  form: {},
-  title: {},
-  input: {},
-  button: {},
+  form: {
+    paddingTop: 92,
+    paddingHorizontal: 16,
+    alignItems: "center",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "white",
+  },
+  title: {
+    marginBottom: 32,
+    fontSize: 30,
+    lineHeight: 35,
+    letterSpacing: 0.01,
+    color: "#212121",
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+    fontSize: 16,
+    lineHeight: 19,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+  },
+  button: {
+    width: "100%",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    paddingVertical: 16,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  },
+  buttonText: {
+    fontStyle: "normal",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "white",
+  },
   text: {},
 });
