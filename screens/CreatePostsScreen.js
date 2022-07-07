371f2722622f8fde7cde9fb838ceea5ecac7e909
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-07 15:24:19 +0300

    Addes create posts screen markup

--- screens/CreatePostsScreen.js
+++ screens/CreatePostsScreen.js
@@ -1,9 +1,45 @@
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+} from "react-native";
+import { Ionicons, Feather } from "@expo/vector-icons";
 
 export default function CreatePostsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>CreatePostsScreen</Text>
+      <View style={styles.contentBlock}>
+        <View style={styles.photoContainer}>
+          <TouchableOpacity style={styles.addPhotoButton} activeOpacity={0.8}>
+            <Ionicons name="camera" size={24} color={"grey"} />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.subtitle}>Download a photo</Text>
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Image name"
+          textContentType="name"
+        />
+        <View style={styles.inputContainer}>
+          <Feather name="map-pin" size={24} style={styles.inputIcon} />
+          <TextInput
+            style={[styles.input, { marginBottom: 0, paddingLeft: 32 }]}
+            placeholder="Location"
+            textContentType="location"
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.submitButton}
+          activeOpacity={0.8}
+          // onPress={handleSubmit}
+        >
+          <Text style={styles.submitButtonText}>Publish</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -11,13 +47,67 @@ export default function CreatePostsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingVertical: 32,
+    paddingHorizontal: 16,
+    backgroundColor: "white",
+  },
+  contentBlock: {
+    marginBottom: 32,
+  },
+  photoContainer: {
+    height: 240,
+    marginBottom: 8,
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 8,
+    backgroundColor: "#E8E8E8",
   },
-  title: {
-    fontFamily: "Roboto-Bold",
-    fontSize: 30,
-    lineHeight: 35,
-    letterSpacing: 0.01,
+  addPhotoButton: {
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    backgroundColor: "white",
+  },
+  subtitle: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+  },
+  inputContainer: {
+    width: "100%",
+    position: "relative",
+    marginBottom: 32,
+  },
+  inputIcon: {
+    position: "absolute",
+    top: 13,
+    color: "#BDBDBD",
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    marginBottom: 16,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    borderBottomWidth: 1,
+    borderColor: "#E8E8E8",
+  },
+  submitButton: {
+    width: "100%",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    paddingVertical: 16,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  },
+  submitButtonText: {
+    fontFamily: "Roboto-Medium",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "white",
   },
 });
