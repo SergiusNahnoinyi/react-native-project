a287d1dd6841d167c3981fa322e53aa63cf0c831
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-14 14:33:36 +0300

    Creates a separate comments list component

---
+++ components/CommentsList.js
@@ -0,0 +1,60 @@
+import { StyleSheet, View, Image, Text, FlatList } from "react-native";
+
+export function CommentsList({ comments }) {
+  return (
+    <FlatList
+      style={styles.commentsList}
+      data={comments}
+      keyExtractor={(comment, index) => index.toString()}
+      renderItem={({ item }) => (
+        <View style={styles.commentsItem}>
+          <Image
+            style={styles.userAvatar}
+            source={require("../../assets/images/avatar.jpg")}
+          />
+          <View style={styles.commentsThumb}>
+            <Text style={styles.commentsText}>{comments}</Text>
+            <Text style={styles.commentsDate}>
+              {new Date().toLocaleString()}
+            </Text>
+          </View>
+        </View>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  commentsList: {
+    marginBottom: 31,
+  },
+  commentsItem: {
+    marginBottom: 24,
+    flexDirection: "row",
+  },
+  userAvatar: {
+    width: 28,
+    height: 28,
+    marginRight: 16,
+    borderRadius: 50,
+  },
+  commentsThumb: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 6,
+    backgroundColor: "#00000008",
+  },
+  commentsText: {
+    marginBottom: 8,
+    fontFamily: "Roboto-Regular",
+    fontSize: 13,
+    lineHeight: 18,
+  },
+  commentsDate: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 10,
+    lineHeight: 12,
+    textAlign: "right",
+    color: "#BDBDBD",
+  },
+});
