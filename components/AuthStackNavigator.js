e1602f0239f21e302d5cd05537e841d163f2dc1b
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-16 06:45:08 +0300

    Moves auth navigation logic to the auth stack component

---
+++ components/AuthStackNavigator.js
@@ -0,0 +1,26 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { RegistrationScreen, LoginScreen } from "../screens/auth";
+
+const AuthStack = createNativeStackNavigator();
+
+export function AuthStackNavigator() {
+  return (
+    <AuthStack.Navigator initialRouteName="Registration">
+      <AuthStack.Screen
+        name="Registration"
+        component={RegistrationScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <AuthStack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </AuthStack.Navigator>
+  );
+}
