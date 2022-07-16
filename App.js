3cb394d3b7e5d47d5fb3202694b5e51a12550dd6
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-16 06:53:15 +0300

    Removes all unnecessary code and paste navigation components

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useFonts } from "expo-font";
-import { Ionicons, Feather } from "@expo/vector-icons";
 
-import { RegistrationScreen, LoginScreen } from "./screens/auth";
-import { MainScreen, CreatePostsScreen, ProfileScreen } from "./screens/main";
-
-const AuthStack = createNativeStackNavigator();
-const BottomTab = createBottomTabNavigator();
+import { AuthStackNavigator } from "./components/AuthStackNavigator";
+import { BottomTabNavigator } from "./components/BottomTabNavigator";
 
 export default function App() {
   const [isAuthorised, setIsAuthorised] = useState(true);
@@ -26,83 +20,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      {!isAuthorised ? (
-        <AuthStack.Navigator initialRouteName="Registration">
-          <AuthStack.Screen
-            name="Registration"
-            component={RegistrationScreen}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <AuthStack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{
-              headerShown: false,
-            }}
-          />
-        </AuthStack.Navigator>
-      ) : (
-        <BottomTab.Navigator
-          initialRouteName={MainScreen}
-          screenOptions={{
-            headerShown: false,
-            headerTitleAlign: "center",
-            headerTitleStyle: {
-              fontFamily: "Roboto-Medium",
-              fontSize: 17,
-              lineHeight: 22,
-            },
-            tabBarStyle: {
-              height: 83,
-              alignItems: "center",
-              paddingTop: 9,
-              paddingBottom: 34,
-            },
-            tabBarItemStyle: {
-              maxHeight: 40,
-              maxWidth: 70,
-              borderRadius: 20,
-            },
-            tabBarShowLabel: false,
-            tabBarActiveTintColor: "white",
-            tabBarActiveBackgroundColor: "#FF6C00",
-          }}
-        >
-          <BottomTab.Screen
-            name="Main"
-            component={MainScreen}
-            options={{
-              headerShown: false,
-              tabBarIcon: ({ focused, size, color }) => (
-                <Ionicons name="grid-outline" size={24} color={color} />
-              ),
-            }}
-          />
-          <BottomTab.Screen
-            name="Create posts"
-            component={CreatePostsScreen}
-            options={{
-              headerShown: true,
-              tabBarHideOnKeyboard: true,
-              tabBarIcon: ({ focused, size, color }) => (
-                <Ionicons name="add-outline" size={24} color={color} />
-              ),
-            }}
-          />
-          <BottomTab.Screen
-            name="Profile"
-            component={ProfileScreen}
-            options={{
-              headerShown: false,
-              tabBarIcon: ({ focused, size, color }) => (
-                <Feather name="user" size={24} color={color} />
-              ),
-            }}
-          />
-        </BottomTab.Navigator>
-      )}
+      {!isAuthorised ? <AuthStackNavigator /> : <BottomTabNavigator />}
     </NavigationContainer>
   );
 }
