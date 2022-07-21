ea41d3f2d2916295c211c84994e2a5edee30805d
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-21 13:46:56 +0300

    Implements keyboard avoiding view for iOS and Android

--- screens/main/CreatePostsScreen.js
+++ screens/main/CreatePostsScreen.js
@@ -109,70 +109,75 @@ export function CreatePostsScreen({ navigation }) {
   return (
     <TouchableWithoutFeedback onPress={hideKeyboard}>
       <View style={styles.container}>
-        {!photo ? (
-          <View style={styles.cameraContainer}>
-            <Camera style={styles.camera} ref={setCameraRef}>
-              <TouchableOpacity
-                style={styles.snapButton}
-                activeOpacity={0.8}
-                onPress={takePhoto}
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" || "android" ? "position" : ""}
+          keyboardVerticalOffset={100}
+        >
+          {!photo ? (
+            <View style={styles.cameraContainer}>
+              <Camera style={styles.camera} ref={setCameraRef}>
+                <TouchableOpacity
+                  style={styles.snapButton}
+                  activeOpacity={0.8}
+                  onPress={takePhoto}
+                >
+                  <Ionicons name="camera" size={24} color={"grey"} />
+                </TouchableOpacity>
+              </Camera>
+              <Text style={styles.subtitle} onPress={takePhoto}>
+                Download a photo
+              </Text>
+            </View>
+          ) : (
+            <View style={styles.previewContainer}>
+              <ImageBackground
+                source={{ uri: photo }}
+                style={styles.previewPhoto}
               >
-                <Ionicons name="camera" size={24} color={"grey"} />
-              </TouchableOpacity>
-            </Camera>
-            <Text style={styles.subtitle} onPress={takePhoto}>
-              Download a photo
-            </Text>
-          </View>
-        ) : (
-          <View style={styles.previewContainer}>
-            <ImageBackground
-              source={{ uri: photo }}
-              style={styles.previewPhoto}
-            >
-              <TouchableOpacity
-                style={styles.editButton}
-                activeOpacity={0.8}
-                onPress={() => setPhoto(null)}
-              >
-                <Ionicons name="camera" size={24} color={"white"} />
-              </TouchableOpacity>
-            </ImageBackground>
-            <Text style={styles.subtitle} onPress={() => setPhoto(null)}>
-              Edit your photo
-            </Text>
-          </View>
-        )}
-        <View style={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeholder="Photo name"
-            textContentType="name"
-            value={photoName}
-            onChangeText={(text) => setPhotoName(text)}
-          />
-          <View style={styles.inputContainer}>
-            <Feather name="map-pin" size={24} style={styles.inputIcon} />
+                <TouchableOpacity
+                  style={styles.editButton}
+                  activeOpacity={0.8}
+                  onPress={() => setPhoto(null)}
+                >
+                  <Ionicons name="camera" size={24} color={"white"} />
+                </TouchableOpacity>
+              </ImageBackground>
+              <Text style={styles.subtitle} onPress={() => setPhoto(null)}>
+                Edit your photo
+              </Text>
+            </View>
+          )}
+          <View style={styles.form}>
             <TextInput
-              style={[styles.input, { marginBottom: 0, paddingLeft: 32 }]}
-              placeholder="Location"
-              textContentType="location"
-              value={location}
-              onChangeText={(text) => setLocation(text)}
+              style={styles.input}
+              placeholder="Photo name"
+              textContentType="name"
+              value={photoName}
+              onChangeText={(text) => setPhotoName(text)}
             />
+            <View style={styles.inputContainer}>
+              <Feather name="map-pin" size={24} style={styles.inputIcon} />
+              <TextInput
+                style={[styles.input, { marginBottom: 0, paddingLeft: 32 }]}
+                placeholder="Location"
+                textContentType="location"
+                value={location}
+                onChangeText={(text) => setLocation(text)}
+              />
+            </View>
+            <TouchableOpacity
+              style={{
+                ...styles.submitButton,
+                backgroundColor: photo ? "#FF6C00" : "#F6F6F6",
+              }}
+              activeOpacity={0.8}
+              onPress={handleSubmit}
+              disabled={photo ? false : true}
+            >
+              <Text style={styles.submitButtonText}>Publish</Text>
+            </TouchableOpacity>
           </View>
-          <TouchableOpacity
-            style={{
-              ...styles.submitButton,
-              backgroundColor: photo ? "#FF6C00" : "#F6F6F6",
-            }}
-            activeOpacity={0.8}
-            onPress={handleSubmit}
-            disabled={photo ? false : true}
-          >
-            <Text style={styles.submitButtonText}>Publish</Text>
-          </TouchableOpacity>
-        </View>
+        </KeyboardAvoidingView>
       </View>
     </TouchableWithoutFeedback>
   );
